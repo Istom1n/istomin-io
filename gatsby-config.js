1c6af6552244c2003edb920bda68b8f3f5e4b3b0
Ivan Istomin <ivan@istomin.im>
2018-05-15 04:40:59 +0300

    Fix confix file

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,47 +31,76 @@ module.exports = {
         },
     },
     plugins: [{
-        resolve: 'gatsby-plugin-feed-generator',
+        resolve: `gatsby-source-filesystem`,
         options: {
-            generator: `GatsbyJS`,
-            rss: true, // Set to false to stop rss generation
-            json: true, // Set to false to stop json feed generation
-            siteQuery: `
-              {
-                site {
-                  siteMetadata {
-                    title
-                    description
-                    siteUrl
-                    author
-                  }
-                }
-              }
-            `,
-            // The plugin requires frontmatter of date, path(or slug/url), and title at minimum
-            feedQuery: `
+            path: `${__dirname}/src/pages`,
+            name: `pages`,
+        },
+    },
+    {
+        resolve: `gatsby-transformer-remark`,
+        options: {
+            plugins: [{
+                    resolve: `gatsby-remark-images`,
+                    options: {
+                        maxWidth: 960,
+                    },
+                },
                 {
-                  allMarkdownRemark(
-                    sort: {order: DESC, fields: [frontmatter___date]}, 
-                    limit: 100, 
-                    
-                    ) {
-                    edges {
-                      node {
-                        html
-                        frontmatter {
-                          date
-                          path
-                          title
-                        }
-                      }
+                    resolve: `gatsby-remark-responsive-iframe`,
+                    options: {
+                        wrapperStyle: `margin-bottom: 1.0725rem`,
                     }
-                  }
-                }
-                `
+                },
+                `gatsby-remark-katex`,
+                `gatsby-remark-prismjs`,
+                `gatsby-remark-copy-linked-files`,
+                `gatsby-remark-smartypants`
+            ]
         }
-    }],
-    plugins: [{
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+        resolve: `gatsby-plugin-google-fonts`,
+        options: {
+            fonts: [`roboto:400,400i,500,700`],
+        }
+    },
+    {
+        resolve: `gatsby-plugin-sitemap`,
+        options: {
+            query: (`
+            {
+                site {
+                siteMetadata {
+                    url
+                }
+                }
+                allSitePage(
+                filter: {
+                    path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+                }) {
+                    edges {
+                    node {
+                        path
+                    }
+                    }
+                }
+            }`),
+            output: `/sitemap.xml`,
+            serialize: ({
+                    site,
+                    allSitePage,
+                }) =>
+                allSitePage.edges.map(edge => ({
+                    url: site.siteMetadata.url + edge.node.path,
+                    changefreq: `daily`,
+                    priority: 0.7,
+                })),
+        },
+    },
+    {
         resolve: `gatsby-plugin-robots-txt`,
         options: {
             host: `https://istomin.io`,
@@ -81,100 +110,71 @@ module.exports = {
                 allow: `/`
             }]
         }
-    }],
-    // plugins: [{
-    //     resolve: `gatsby-plugin-yandex-metrika`,
-    //     options: {
-    //         trackingId: 48754313,
-    //         webvisor: true,
-    //         trackHash: true,
-    //         version: 2,
-    //     },
-    // }],
-    plugins: [{
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/pages`,
-                name: `pages`,
-            },
+    },
+    {
+        resolve: `gatsby-plugin-yandex-metrika`,
+        options: {
+            trackingId: '48754313',
+            webvisor: true,
+            trackHash: true,
         },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [{
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 960,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        }
-                    },
-                    `gatsby-remark-katex`,
-                    `gatsby-remark-prismjs`,
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`
-                ]
+    },
+    {
+        resolve: `gatsby-plugin-feed`,
+        options: {
+          query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                  site_url: siteUrl
+                }
+              }
             }
-        },
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-plugin-google-fonts`,
-            options: {
-                fonts: [`roboto:400,400i,500,700`],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-sitemap`,
-            options: {
-                query: (`
+          `,
+          feeds: [
+            {
+              serialize: ({ query: { site, allMarkdownRemark } }) => {
+                return allMarkdownRemark.edges.map(edge => {
+                  return Object.assign({}, edge.node.frontmatter, {
+                    description: edge.node.excerpt,
+                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                    custom_elements: [{ "content:encoded": edge.node.html }],
+                  });
+                });
+              },
+              query: `
                 {
-                  site {
-                    siteMetadata {
-                      url
-                    }
-                  }
-                  allSitePage(
-                    filter: {
-                      path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
-                    }) {
-                      edges {
-                        node {
-                          path
+                  allMarkdownRemark(
+                    limit: 1000,
+                    sort: { order: DESC, fields: [frontmatter___date] },
+                    filter: {frontmatter: { draft: { ne: true } }}
+                  ) {
+                    edges {
+                      node {
+                        excerpt
+                        html
+                        fields { slug }
+                        frontmatter {
+                          title
+                          date
                         }
                       }
+                    }
                   }
-                }`),
-                output: `/sitemap.xml`,
-                serialize: ({
-                        site,
-                        allSitePage,
-                    }) =>
-                    allSitePage.edges.map(edge => ({
-                        url: site.siteMetadata.url + edge.node.path,
-                        changefreq: `daily`,
-                        priority: 0.7,
-                    })),
+                }
+              `,
+              output: "/rss.xml",
             },
+          ],
         },
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-catch-links`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-postcss-sass`,
-        `gatsby-plugin-robots-txt`,
-        // {
-        //     resolve: `gatsby-plugin-nprogress`,
-        //     options: {
-        //         // Setting a color is optional.
-        //         color: `tomato`,
-        //         // Disable the loading spinner.
-        //         showSpinner: true,
-        //     },
-        // },
-
+      },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss-sass`,
     ]
 }
